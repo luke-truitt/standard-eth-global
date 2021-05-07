d80023c27c60c1f9a040abbd1d231fa72949a432
Mary Gooneratne <mmg53@duke.edu>
2021-05-07 11:04:02 -0400

    here

--- playground/maticwallet.js
+++ playground/maticwallet.js
@@ -1,55 +1,62 @@
 const Web3 = require("web3");
 const web3 = new Web3("https://rpc-mumbai.matic.today");
 const { ethers } = require("ethers");
-import Matic from "maticjs";
+// import Matic from "maticjs";
 
-const matic = new Matic(options);
+// const matic = new Matic(options);
 
 const Wallet = ethers.Wallet;
 const utils = ethers.utils;
 const mnemonic =
-  "announce room limb pattern dry unit scale effort smooth jazz weasel alcohol";
+  "forest burst jungle inch report alone clock unlock then copy panel come";
+const mn1 =
+  "agree bacon favorite electric cover whip labor brave cry kitchen rose current";
 wallet = Wallet.fromMnemonic(mnemonic);
+wallet2 = Wallet.fromMnemonic(mnemonic);
+const recipient = wallet2.address;
 
-const recipient = '"0x8ba1f109551bD432803012645Ac136ddd64DBA72"';
-const token = config.MUMBAI_ERC20; // test token address
-// const token = config.MUMBAI_WETH
-const amount = "1000000000000000000"; // amount in wei
+// const recipient = '"0x8ba1f109551bD432803012645Ac136ddd64DBA72"';
+// const token = config.MUMBAI_ERC20; // test token address
+// // const token = config.MUMBAI_WETH
+// const amount = "1000000000000000000"; // amount in wei
 
-matic.initialize().then(() => {
-  matic.setWallet(wallet.privateKey);
-  // Transfer ERC20 Tokens
-  matic
-    .transferERC20Tokens(token, recipient, amount, {
-      from,
-    })
-    .then((res) => {
-      console.log("hash", res.transactionHash);
-    });
-});
-
-console.log(wallet.address);
-console.log(wallet.privateKey);
-// // true
-const provider = new ethers.providers.JsonRpcProvider(
-  "https://rpc-mumbai.maticvigil.com/",
-  {
-    name: "Matic Testnet",
-    chainId: 80001,
-  }
-);
-tx = {
-  to:" "
-  value: utils.parseEther("1.0"),
-};
-
-// const wallet.signTransaction(tx)
-// const connectedWallet = wallet.connect(provider);
-// connectedWallet.getBalance().then((promise) => {
-//   console.log(promise);
+// matic.initialize().then(() => {
+//   matic.setWallet(wallet.privateKey);
+//   // Transfer ERC20 Tokens
+//   matic
+//     .transferERC20Tokens(token, recipient, amount, {
+//       from,
+//     })
+//     .then((res) => {
+//       console.log("hash", res.transactionHash);
+//     });
 // });
 
-// .then((promise) => {
+console.log(wallet2.address);
+console.log(wallet.privateKey);
+// const provider = new ethers.providers.JsonRpcProvider(
+//   "https://rpc-mumbai.maticvigil.com/",
+//   {
+//     name: "Matic Testnet",
+//     chainId: 80001,
+//   }
+// );
+
+// tx = {
+//   to: recipient,
+//   value: utils.parseEther("1.0"),
+// };
+
+// const connectedWallet = wallet.connect(provider);
+// connectedWallet
+//   .getBalance()
+//   .then((promise) => {
+//     console.log(promise);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+// wallet.signTransaction(tx).then((promise) => {
 //   console.log(promise);
 //   const connectedWallet = wallet.connect(provider);
 //   connectedWallet.getBalance().then((promise) => {
@@ -57,6 +64,6 @@ tx = {
 //   });
 // });
 
-function createWallet(mnemoic) {
-  return Wallet.fromMnemonic(mnemonic);
-}
+// function createWallet(mnemoic) {
+//   return Wallet.fromMnemonic(mnemonic);
+// }
